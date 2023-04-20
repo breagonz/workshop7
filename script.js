b8cca8f5698d759f86600b59708f0584acc1d477
Breanna <b.gonzales21@yahoo.com>
2023-04-19 17:24:38 -0700

    phase 2 done

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ function calculate(operation) {
     result = num1 + num2
     
     document.getElementById("result").textContent = result;
+
+    // phase 2 
+
+    const input3 = document.getElementById("input3").value;
+    const input4 = document.getElementById("input4").value;
+    let result2;
+
+    const num3 = parseFloat(input3);
+    const num4 = parseFloat(input4);
+    result = num3 - num4
+    
+    document.getElementById("result2").textContent = result;
 }
